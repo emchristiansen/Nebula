5486a2ee5b6d7f0e0d26f03bdc5a9229e8b62d04
Eric Christiansen <ericmartinchristiansen@gmail.com>
2012-08-31 20:52:22 -0700

    added RichImage, with the purpose of adding easy access to BufferedImage pixels

--- scala/native/src/main/scala/image_processing.scala
+++ scala/native/src/main/scala/image_processing.scala
@@ -16,6 +16,53 @@ import scala.Array.fallbackCanBuildFrom
 
 import com.googlecode.javacv.cpp.opencv_features2d.KeyPoint
 
+case class RichImage(image: BufferedImage) {
+  def getPixel(x: Int, y: Int): Pixel = {
+    require(x >= 0 && x < image.getWidth)
+    require(y >= 0 && y < image.getHeight)
+    Pixel.getPixel(image, x, y)
+  }
+  
+  // Uses linear interpolation.
+  def getSubPixel(x: Double, y: Double): Pixel = {
+    require(x >= 0 && x < image.getWidth)
+    require(y >= 0 && y < image.getHeight)    
+    
+    def intsAndWeights(double: Double): List[Tuple2[Int, Double]] = {
+      val floor = double.floor
+      if (floor == double) {
+        List((double.toInt, 1.0))
+      } else {
+        val floorWeight = 1 - (double - floor)
+        val ceil = double.ceil
+        val ceilWeight = 1 - (ceil - double) - (ceil - floor)
+        List((floor.toInt, floorWeight), (ceil.toInt, ceilWeight))
+      }
+    }
+    
+    val xIntsAndWeights = intsAndWeights(x)
+    val yIntsAndWeights = intsAndWeights(y)
+    
+    val summands = for (
+      (xInt, xWeight) <- xIntsAndWeights;
+      (yInt, yWeight) <- yIntsAndWeights
+    ) yield {
+      val weight = xWeight * yWeight
+      val pixel = getPixel(xInt, yInt)
+      
+      val Pixel(a, r, g, b) = pixel
+      List(weight * a, weight * r, weight * g, weight * b)
+    }
+    
+    val List(a, r, g, b) = summands.transpose.map(_.sum)
+    Pixel(a.toInt, r.toInt, g.toInt, b.toInt)
+  }
+}
+
+object RichImage {
+  implicit def bufferedImage(image: BufferedImage): RichImage = RichImage(image)
+}
+
 object ImageProcessing {
   def boxBlur(boxWidth: Int, image: BufferedImage): BufferedImage = {
     val kernel = {
