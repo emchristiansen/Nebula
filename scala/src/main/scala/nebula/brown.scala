9620a0668cc455acdec1080e82554ae2f8a385fe
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-01-26 14:33:26 -0800

    realized the StorageInfo impl is generic, so moved it. Also added extra file for Brown dataset classes

--- scala/src/main/scala/nebula/brown.scala
+++ scala/src/main/scala/nebula/brown.scala
@@ -21,6 +21,12 @@ object BrownExperiment {
       implicit runtimeConfig: RuntimeConfig) extends ExperimentRunner[BrownExperimentResults[E, M, F]] {
     override def run = BrownExperimentResults(self)
   }
+
+  implicit def brownExperiment2ExperimentRunner[E <% Extractor[F], M <% Matcher[F], F](
+    self: BrownExperiment[E, M, F]): RuntimeConfig => ExperimentRunner[BrownExperimentResults[E, M, F]] = runtimeConfig => {
+    implicit val iRC = runtimeConfig
+    self
+  }
 }
 
 ///////////////////////////////////////////////////////////
@@ -50,80 +56,3 @@ object BrownExperimentResults {
   }
 }
 
-case class BrownPatch(image: BufferedImage, id: Int)
-
-case class PatchPair(left: BrownPatch, right: BrownPatch)
-
-object PatchPair {
-  implicit class PatchPairOps(self: PatchPair) {
-    def corresponds = self.left.id == self.right.id
-
-    def getDistance[E <% Extractor[F], M <% Matcher[F], F](extractor: E, matcher: M): Option[Double] = {
-      val PatchPair(left, right) = self
-      val leftDescriptor = extractDescriptorAtCenter(extractor, left.image)
-      val rightDescriptor = extractDescriptorAtCenter(extractor, right.image)
-      for (l <- leftDescriptor; r <- rightDescriptor) yield matcher.distance(l, r)
-    }
-  }
-
-  def extractDescriptorAtCenter[E <% Extractor[F], F](extractor: E, image: BufferedImage): Option[F] = {
-    require(image.getWidth == 64)
-    require(image.getHeight == 64)
-
-    val keyPoint = KeyPointUtil(32, 32)
-    extractor.extractSingle(image, keyPoint)
-  }
-
-  def loadPatchPairs(datasetName: String, numMatches: Int, dataRoot: File): Stream[PatchPair] = {
-    val directory = new File(dataRoot, s"brownImages/${datasetName}").mustExist
-
-    val manifest = new File(directory, s"m50_${numMatches}_${numMatches}_0.txt").mustExist
-
-    val lines = FileUtils.readFileToString(manifest).split("\n")
-    assert(lines.size == numMatches)
-
-    lines.toStream.map(manifestLineToPatchPair(directory))
-  }
-
-  def manifestLineToPatchPair(directory: File)(line: String): PatchPair = {
-    val (leftIndex, leftID, rightIndex, rightID) = parseManifestLine(line)
-
-    val load = loadPatch(directory) _
-    val leftImage = load(leftIndex)
-    val rightImage = load(rightIndex)
-
-    PatchPair(BrownPatch(leftImage, leftID), BrownPatch(rightImage, rightID))
-  }
-
-  def parseManifestLine(line: String): Tuple4[Int, Int, Int, Int] = {
-    val Parser = """(\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)""".r
-    val Parser(leftIndex, leftID, _, rightIndex, rightID, _, _) = line
-    return (leftIndex.toInt, leftID.toInt, rightIndex.toInt, rightID.toInt)
-  }
-
-  def loadPatch(directory: File)(index: Int): BufferedImage = {
-    val numPatchesPerFile = 256
-
-    val imageIndex = index / numPatchesPerFile
-    assert(imageIndex >= 0)
-    val imageFile = new File(directory, f"patches${imageIndex}%04d.bmp").mustExist
-    val image = ImageIO.read(imageFile)
-
-    val patchIndex = index - numPatchesPerFile * imageIndex
-    assert(patchIndex >= 0)
-    assert(patchIndex < numPatchesPerFile)
-    val numPatchesPerRow = 16
-    val patchRow = patchIndex / numPatchesPerRow
-    assert(patchRow >= 0)
-    assert(patchRow < numPatchesPerFile / numPatchesPerRow)
-    val patchColumn = patchIndex - numPatchesPerRow * patchRow
-    assert(patchColumn >= 0)
-    assert(patchColumn < numPatchesPerRow)
-
-    // Patches are square.
-    val patchWidth = 64
-    val xBegin = patchWidth * patchColumn
-    val yBegin = patchWidth * patchRow
-    image.getSubimage(xBegin, yBegin, patchWidth, patchWidth)
-  }
-}
--- scala/src/main/scala/nebula/results.scala
+++ scala/src/main/scala/nebula/results.scala
@@ -1,6 +1,9 @@
 package nebula
 
 import java.io.File
+import spray.json._
+import org.apache.commons.io.FileUtils
+import nebula.util._
 
 ///////////////////////////////////////////////////////////
 
@@ -14,6 +17,60 @@ trait StorageInfo[R] {
   def save: R => Unit
   def load: Option[R]
   def name: String
+}
+
+object StorageInfo {
+  implicit class Experiment2StorageInfoWithRuntimeConfig[E <% ExperimentRunner[R]: JsonFormat, R: JsonFormat](
+    experiment: E)(
+      implicit runtimeConfig: RuntimeConfig) extends StorageInfo[R] {
+    override def currentPath: File = new File(outDirectory, filename)
+
+    override def mostRecentPath: Option[File] = existingResultsFiles.headOption
+
+    override def save = results => {
+      println(s"Saving to ${currentPath}")
+      val json = results.toJson
+      org.apache.commons.io.FileUtils.writeStringToFile(currentPath, json.prettyPrint)
+    }
+
+    override def load = mostRecentPath map { file =>
+      println(s"Loading ${file}")
+      val jsonString = FileUtils.readFileToString(file)
+      jsonString.asJson.convertTo[R]
+    }
+
+    override def name = unixEpoch + "_" + nameNoTime
+
+    ///////////////////////////////////////////////////////
+
+    val unixEpoch = System.currentTimeMillis / 1000L
+
+    def nameNoTime: String = {
+      val fullString = JSONUtil.flattenJson(experiment.toJson)
+      //      // Unfortunately, this string is too long to be part of a filename.
+      //      fullString.take(100) + "_" + fullString.hashCode
+      fullString
+    }
+
+    def filenameNoTime: String = nameNoTime + ".json"
+
+    def filename: String = unixEpoch + "_" + filenameNoTime
+
+    def outDirectory: File = runtimeConfig.projectChildPath(
+      "results/experiment_data/")
+
+    def existingResultsFiles: Seq[File] = {
+      val allPaths = outDirectory.list.toList.map(path => outDirectory + "/" + path.toString)
+      val matchingPaths = allPaths.filter(_.contains(filenameNoTime))
+      matchingPaths.sortBy(identity).reverse.map(path => new File(path))
+    }
+  }
+
+  implicit def experiment2StorageInfo[E <% ExperimentRunner[R]: JsonFormat, R: JsonFormat](
+    experiment: E): RuntimeConfig => StorageInfo[R] = runtimeConfig => {
+    implicit val iRC = runtimeConfig
+    experiment
+  }
 }
 
 
