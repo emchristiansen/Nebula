f50c934ef027d112d349d9505ec1c9ed4bd9f162
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-05-10 23:39:30 -0700

    no idea what I did last

--- scala/src/main/scala/nebula/util/geometry.scala
+++ scala/src/main/scala/nebula/util/geometry.scala
@@ -22,6 +22,8 @@ import nebula.graveyard.Point2D
 import nebula.imageProcessing.ImageUtil
 import nebula.imageProcessing.RichImage.bufferedImage
 import nebula._
+import MathUtil._
+import org.apache.commons.math3.linear._
 
 ///////////////////////////////////////////////////////////
 
@@ -36,6 +38,45 @@ case class Homography(matrix: RealMatrix) {
     val outHomogeneous = matrix.operate(inHomogeneous)
     Geometry.dehomogenize(outHomogeneous)
   }
+
+  def transformXYOnly(in: KeyPoint): KeyPoint = {
+    val inVector = new ArrayRealVector(Array(in.pt.x, in.pt.y))
+    val outVector = transform(inVector)
+    new KeyPoint(
+      outVector.getEntry(0).toFloat,
+      outVector.getEntry(1).toFloat,
+      0)
+  }
+
+  def transform(left: KeyPoint): KeyPoint = {
+    val leftRay = Geometry.getRayPoint(left)
+
+    val right = transformXYOnly(left)
+    val rightRay = transformXYOnly(leftRay)
+
+    def getScaleAndRotation(base: KeyPoint, tip: KeyPoint): ScaleAndRotation = {
+      val offsetX = tip.pt.x - base.pt.x
+      val offsetY = tip.pt.y - base.pt.y
+
+      val scale = MathUtil.l2Norm(Array(offsetX, offsetY))
+      val rotation = math.atan2(offsetY, offsetX)
+      ScaleAndRotation(scale, rotation mod (2 * math.Pi))
+    }
+
+    // We use the difference in scale and rotation to determine the
+    // scale and rotation of the output KeyPoint.
+    val leftSimilarity = getScaleAndRotation(left, leftRay)
+    val rightSimilarity = getScaleAndRotation(right, rightRay)
+
+    val rightSize =
+      left.size * rightSimilarity.scale / leftSimilarity.scale
+    new KeyPoint(
+      right.pt.x.toFloat,
+      right.pt.y.toFloat,
+      rightSize.toFloat,
+      rightSimilarity.rotation.toFloat)
+  }
+
 }
 
 object Homography {
@@ -66,7 +107,7 @@ object ImageGeometry {
         val pointAfter = rotated.getSubPixel(keyPoint.pt.x, keyPoint.pt.y)
         asserty(pointBefore.isDefined)
         asserty(pointAfter.isDefined)
-//        asserty(pointBefore.get.isSimilar(5, pointAfter.get))
+        //        asserty(pointBefore.get.isSimilar(5, pointAfter.get))
 
         rotated
       }
@@ -78,7 +119,7 @@ object ImageGeometry {
       if (scaleFactor == 1) image
       else {
         val ((scaleFactorX, scaleFactorY), scaledImage) = ImageUtil.scale(scaleFactor, image)
-        
+
         val translateOp = new AffineTransformOp(
           AffineTransform.getTranslateInstance(
             keyPoint.pt.x - scaleFactorX * keyPoint.pt.x,
@@ -99,7 +140,194 @@ object ImageGeometry {
   }
 }
 
+case class ScaleAndRotation(scale: Double, rotation: Double) {
+  asserty(scale > 0)
+  asserty(rotation >= 0)
+  asserty(rotation < 2 * math.Pi)
+}
+
 object Geometry {
+  /**
+   * The angle and scale is represented as an extra KeyPoint, located
+   * a distance proportional to the KeyPoint size along the ray
+   * given by the KeyPoint angle.
+   */
+  def getRayPoint(keyPoint: KeyPoint): KeyPoint = {
+    // Used to ensure locality. Smaller is better, but too small
+    // means precision problems.
+    val epsilon = 0.1
+    val offset = keyPoint.size * epsilon
+
+    // This part is tricky, because we need to be sure the coordinate
+    // systems are consistent.
+    val x = keyPoint.pt.x + offset * math.cos(keyPoint.angle)
+    val y = keyPoint.pt.y + offset * math.sin(keyPoint.angle)
+
+    new KeyPoint(x.toFloat, y.toFloat, 0)
+  }
+
+  //  def scaleAndRotationOfHomographyAtPoint(
+  //    homography: Homography,
+  //    point: KeyPoint): ScaleAndRotation = {
+  //    
+  //  }
+
+  /**
+   * Homography from four points.
+   */
+  def fitHomographyFromFourTranslations(
+    leftKeyPoints: Seq[KeyPoint],
+    rightKeyPoints: Seq[KeyPoint]): Homography = {
+    def validate(keyPoints: Seq[KeyPoint]) {
+      requirey(keyPoints.size == 4)
+    }
+    validate(leftKeyPoints)
+    validate(rightKeyPoints)
+
+    // From Hartley and Zisserman "Multiple View Geometry in Computer Vision" p89.
+    def keyPointPairToConstraint(
+      leftKeyPoint: KeyPoint,
+      rightKeyPoint: KeyPoint): RealMatrix = {
+      val x: Double = leftKeyPoint.pt.x
+      val y: Double = leftKeyPoint.pt.y
+      val w: Double = 1
+
+      val xp: Double = rightKeyPoint.pt.x
+      val yp: Double = rightKeyPoint.pt.y
+      val wp: Double = 1
+
+      val data = Array[Double](
+        0, 0, 0, -wp * x, -wp * y, -wp * w, yp * x, yp * y, yp * w,
+        wp * x, wp * y, wp * w, 0, 0, 0, -xp * x, -xp * y, -xp * w).grouped(9).toArray
+
+      val matrix = new Array2DRowRealMatrix(data)
+      asserty(matrix.getRowDimension == 2)
+      asserty(matrix.getColumnDimension == 9)
+      matrix
+    }
+
+    val constraints =
+      (leftKeyPoints zip rightKeyPoints) map {
+        case (left, right) => keyPointPairToConstraint(left, right)
+      }
+
+    def concatenateVertical(left: RealMatrix, right: RealMatrix): RealMatrix = {
+      new Array2DRowRealMatrix(left.getData ++ right.getData)
+    }
+
+    val constraint = constraints.reduce(concatenateVertical)
+    asserty(constraint.getRowDimension == 8)
+    asserty(constraint.getColumnDimension == 9)
+
+    val qr = new QRDecomposition(constraint.transpose)
+    val q = qr.getQ
+    // We want the last column of Q.
+    val data = for (row <- 0 until 9) yield q.getEntry(row, 8) / q.getEntry(8, 8)
+
+    val matrix = new Array2DRowRealMatrix(data.toArray.grouped(3).toArray)
+    val homography = Homography(matrix)
+
+    //    // From Hartley and Zisserman "Multiple View Geometry in Computer Vision" p91,
+    //    // inhomogeneous solution.
+    //    def keyPointPairToConstraintMatrices(
+    //      leftKeyPoint: KeyPoint,
+    //      rightKeyPoint: KeyPoint): (RealMatrix, RealMatrix) = {
+    //      val x: Double = leftKeyPoint.pt.x
+    //      val y: Double = leftKeyPoint.pt.y
+    //      val w: Double = 1
+    //
+    //      val xp: Double = rightKeyPoint.pt.x
+    //      val yp: Double = rightKeyPoint.pt.y
+    //      val wp: Double = 1
+    //
+    //      val left = {
+    //        val data = Array[Double](
+    //          0, 0, 0, -x * wp, -y * wp, -w * wp, x * yp, y * yp,
+    //          x * wp, y * wp, w * wp, 0, 0, 0, -x * xp, -y * xp).grouped(8).toArray
+    //        val matrix = new Array2DRowRealMatrix(data)
+    //        asserty(matrix.getRowDimension == 2)
+    //        asserty(matrix.getColumnDimension == 8)
+    //        matrix
+    //      }
+    //
+    //      val right = {
+    //        val data = Array[Double](
+    //          -w * yp,
+    //          w * xp).grouped(1).toArray
+    //        val matrix = new Array2DRowRealMatrix(data)
+    //        asserty(matrix.getRowDimension == 2)
+    //        asserty(matrix.getColumnDimension == 1)
+    //        matrix
+    //      }
+    //
+    //      (left, right)
+    //    }
+    //
+    //    val constraintPairs =
+    //      (leftKeyPoints zip rightKeyPoints) map {
+    //        case (left, right) => keyPointPairToConstraintMatrices(left, right)
+    //      }
+    //
+    //    def concatenateVertical(left: RealMatrix, right: RealMatrix): RealMatrix = {
+    //      new Array2DRowRealMatrix(left.getData ++ right.getData)
+    //    }
+    //
+    //    val leftConstraint = constraintPairs.map(_._1).reduce(concatenateVertical)
+    //    asserty(leftConstraint.getRowDimension == 8)
+    //    asserty(leftConstraint.getColumnDimension == 8)
+    //
+    //    val rightConstraint = constraintPairs.map(_._2).reduce(concatenateVertical)
+    //    asserty(rightConstraint.getRowDimension == 8)
+    //    asserty(rightConstraint.getColumnDimension == 1)
+    //
+    //    val solver = new SingularValueDecomposition(leftConstraint).getSolver
+    //    val hStacked = solver.solve(rightConstraint)
+    //    
+    //    val homography = {
+    //      val data: Array[Double] = hStacked.getData.flatten.toArray ++ Array(1.0)
+    //      val matrix = new Array2DRowRealMatrix(data.grouped(3).toArray)
+    //      Homography(matrix)
+    //    }
+
+//    for ((leftKeyPoint, rightKeyPoint) <- leftKeyPoints zip rightKeyPoints) {
+//      val projected = homography.transformXYOnly(leftKeyPoint)
+//      implicit def epsilon = Epsilon(1.0)
+//      assertNear(projected.pt.x, rightKeyPoint.pt.x)
+//      assertNear(projected.pt.y, rightKeyPoint.pt.y)
+//    }
+
+    homography
+  }
+
+  /**
+   * Two correspondences with know scale and angle change are used
+   * to fit a homography.
+   * The homography is from the left to the right.
+   */
+  def fitHomographyFromTwoSimilarityCorrespondences(
+    left: Seq[KeyPoint],
+    right: Seq[KeyPoint]): Homography = {
+    def validate(keyPoints: Seq[KeyPoint]) = {
+      asserty(keyPoints.size == 2)
+      keyPoints foreach { keyPoint =>
+        asserty(keyPoint.size > 0)
+        asserty(keyPoint.angle >= 0)
+        asserty(keyPoint.angle < 2 * math.Pi)
+      }
+    }
+    validate(left)
+    validate(right)
+
+    def addRayPoint(keyPoint: KeyPoint): Seq[KeyPoint] = {
+      Seq(keyPoint, getRayPoint(keyPoint))
+    }
+
+    val leftFour = left.flatMap(addRayPoint)
+    val rightFour = right.flatMap(addRayPoint)
+
+    fitHomographyFromFourTranslations(leftFour, rightFour)
+  }
+
   def homogenize(inhomogeneous: RealVector): RealVector =
     inhomogeneous.append(new ArrayRealVector(Array(1.0)))
 
--- scala/src/test/scala/nebula/util/testGeometry.scala
+++ scala/src/test/scala/nebula/util/testGeometry.scala
@@ -10,40 +10,210 @@ import org.scalatest.junit.JUnitRunner
 import nebula.util._
 import nebula.util.ImageGeometry._
 import nebula.imageProcessing._
+import org.apache.commons.math3.linear.Array2DRowRealMatrix
+import org.apache.commons.math3.linear.ArrayRealVector
+import org.apache.commons.math3.linear.LUDecomposition
+import org.apache.commons.math3.linear.MatrixUtils
+import org.apache.commons.math3.linear.RealMatrix
+import org.apache.commons.math3.linear.RealVector
+import org.apache.commons.math3.linear.SingularValueDecomposition
+import org.opencv.features2d.KeyPoint
+import java.awt.geom.AffineTransform
+import org.scalacheck._
+import org.scalatest.prop._
+import org.scalatest._
+import nebula.testing._
+import scala.util.Random
 
 ///////////////////////////////////////////////////////////
 
 @RunWith(classOf[JUnitRunner])
 class TestGeometry extends FunSuite {
   val image = ImageIO.read(new File(getClass.getResource("/iSpy.png").getFile).mustExist)
-  
+
   val center = KeyPointUtil(image.getWidth.toFloat / 2, image.getHeight.toFloat / 2)
-  
-  test("rotateAboutPoint", FastTest, InteractiveTest) {
+
+//  val genKeyPoint = Generators.genKeyPoint(image.getWidth, image.getHeight, 100)
+
+  test("fit lots of homographies", FastTest) {
+    def randomKeyPoint = new KeyPoint(
+      1000 * new Random().nextFloat,
+      1000 * new Random().nextFloat,
+      0,
+      0)
+
+    def random4 = 4 times randomKeyPoint
+
+    for (_ <- 0 until 20) {
+      val leftKeyPoints = random4 
+      val rightKeyPoints = random4
+
+      val homography = Geometry.fitHomographyFromFourTranslations(
+        leftKeyPoints,
+        rightKeyPoints)
+
+//      println(homography)
+    }
+
+    //    implicit val generatorDrivenConfig =
+    //      PropertyCheckConfig(minSuccessful = 20, maxSize = 5)
+    //    forAll(Gen.listOfN(8, genKeyPoint)) { keyPoints =>
+    //      whenever(keyPoints.size == 8) {
+    //
+    //      }
+    //    }
+  }
+
+  test("homography from 4 points", FastTest) {
+    val golden = {
+      val matrix = new Array2DRowRealMatrix(Array[Double](
+        2.0, 0.1, -5,
+        -0.4, 0.6, 10,
+        0, 0, 1).grouped(3).toArray)
+      Homography(matrix)
+    }
+
+    def keyPointFromXY(x: Double, y: Double): KeyPoint = new KeyPoint(
+      x.toFloat,
+      y.toFloat,
+      0,
+      0,
+      0,
+      0)
+
+    val leftKeyPoints = Seq(
+      keyPointFromXY(0, 0),
+      keyPointFromXY(0, 32),
+      keyPointFromXY(10, 0),
+      keyPointFromXY(11, 11))
+
+    val rightKeyPoints = leftKeyPoints map (golden.transformXYOnly)
+
+    val homography = Geometry.fitHomographyFromFourTranslations(
+      leftKeyPoints,
+      rightKeyPoints)
+
+    println(golden)
+    println(homography)
+  }
+
+  test("homography from 2 points", FastTest) {
+    val golden = {
+      val matrix = new Array2DRowRealMatrix(Array[Double](
+        2.0, 0.1, -5,
+        -0.4, 0.6, 10,
+        0.2, -0.3, 1).grouped(3).toArray)
+      Homography(matrix)
+    }
+
+    println(golden)
+
+    val leftA = new KeyPoint(
+      1,
+      0,
+      1,
+      0)
+
+    val rightA = golden.transform(leftA)
+
+    val leftB = new KeyPoint(
+      1,
+      1,
+      1,
+      0)
+
+    val rightB = golden.transform(leftB)
+
+    println(leftA)
+    println(leftB)
+    println(rightA)
+    println(rightB)
+
+    val homography = Geometry.fitHomographyFromTwoSimilarityCorrespondences(
+      Seq(leftA, leftB),
+      Seq(rightA, rightB))
+
+    println(homography)
+  }
+
+  //  ignore("homography from 2 points", FastTest) {
+  //    val transform = {
+  //      val base = new AffineTransform
+  //      base.rotate(math.Pi / 2)
+  //      base.scale(3, 3)
+  //      base
+  //    }
+  //
+  //    val golden = {
+  //      val flatMatrix = Array[Double](0, 0, 0, 0, 0, 0)
+  //      transform.getMatrix(flatMatrix)
+  //      val columnMajor = Array(
+  //        flatMatrix(0), flatMatrix(1), 0,
+  //        flatMatrix(2), flatMatrix(3), 0,
+  //        flatMatrix(4), flatMatrix(5), 1)
+  //      val data = new Array2DRowRealMatrix(
+  //        columnMajor.grouped(3).toArray.transpose)
+  //      Homography(data)
+  //    }
+  //
+  //    println(golden)
+  //
+  //    val leftA = new KeyPoint(
+  //      1,
+  //      0,
+  //      1,
+  //      0)
+  //
+  //    val rightA = new KeyPoint(
+  //      0,
+  //      3,
+  //      3,
+  //      (math.Pi / 2).toFloat)
+  //
+  //    val leftB = new KeyPoint(
+  //      2,
+  //      0,
+  //      1,
+  //      0)
+  //
+  //    val rightB = new KeyPoint(
+  //      0,
+  //      6,
+  //      3,
+  //      (math.Pi / 2).toFloat)
+  //
+  //    val homography = Geometry.fitHomographyFromTwoSimilarityCorrespondences(
+  //      Seq(leftA, leftB),
+  //      Seq(rightA, rightB))
+  //      
+  //    println(homography)
+  //  }
+
+  ignore("rotateAboutPoint", FastTest, InteractiveTest) {
     for (theta <- 0.0 until 2 * math.Pi by math.Pi / 16) {
       val rotated = image.rotateAboutPoint(theta, center)
       val patch = ImageUtil.getSubimageCenteredAtPoint(
-          rotated,
-          center.pt.x,
-          center.pt.y,
-          20,
-          20)
+        rotated,
+        center.pt.x,
+        center.pt.y,
+        20,
+        20)
       TestUtil.dumpImage(f"rotated_${theta}%.2f.png", TestUtil.scale10(patch))
     }
   }
-  
-  test("scaleAboutPoint", FastTest, InteractiveTest) {
+
+  ignore("scaleAboutPoint", FastTest, InteractiveTest) {
     val exponents = 0.2 until 3.0 by 0.05
     val scaleFactors = exponents.map(e => math.pow(2, e))
     for (scaleFactor <- scaleFactors) {
       val scaled = image.scaleAboutPoint(scaleFactor, center)
       val patch = ImageUtil.getSubimageCenteredAtPoint(
-          scaled,
-          center.pt.x,
-          center.pt.y,
-          20,
-          20)
-      TestUtil.dumpImage(f"scaled_${scaleFactor}%.2f.png", TestUtil.scale10(patch))      
+        scaled,
+        center.pt.x,
+        center.pt.y,
+        20,
+        20)
+      TestUtil.dumpImage(f"scaled_${scaleFactor}%.2f.png", TestUtil.scale10(patch))
     }
   }
 }
